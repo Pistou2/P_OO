f921e896f24f552d8c04a3c4f4bcd6eaeeab45ae
Yann Merk <yannmerk@gmail.com>
2017-01-11 10:23:48 +0100

    Create a little Brainstorm (with Freeplane)

---
+++ Brainstorm.mm
@@ -0,0 +1,138 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Smart Th&#xe9;saurus" FOLDED="false" ID="ID_1478742780" CREATED="1484125002302" MODIFIED="1484125623497" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="1.5">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="8" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Recherche" POSITION="right" ID="ID_733377681" CREATED="1484125057557" MODIFIED="1484125064568">
+<edge COLOR="#00ff00"/>
+<node TEXT="par Nom" ID="ID_1677388473" CREATED="1484125398927" MODIFIED="1484125406235"/>
+<node TEXT="par Date" ID="ID_906254892" CREATED="1484125407184" MODIFIED="1484125411410"/>
+<node TEXT="par Autheur" ID="ID_1641815992" CREATED="1484125412240" MODIFIED="1484125418277"/>
+<node TEXT="Avanc&#xe9;" ID="ID_1398735557" CREATED="1484125588590" MODIFIED="1484125591017">
+<node TEXT="? par contenu (document texte) ?" ID="ID_237270544" CREATED="1484125418825" MODIFIED="1484125436853"/>
+</node>
+</node>
+<node TEXT="Indexation" POSITION="left" ID="ID_1606170696" CREATED="1484125068607" MODIFIED="1484125119340">
+<edge COLOR="#ff00ff"/>
+<node TEXT="K:/" ID="ID_1293882937" CREATED="1484125075815" MODIFIED="1484125090338">
+<node TEXT="R&#xe9;cursif" ID="ID_1784907076" CREATED="1484125247696" MODIFIED="1484125250779"/>
+</node>
+<node TEXT="etml.ch" ID="ID_1261956186" CREATED="1484125085802" MODIFIED="1484125095564">
+<node TEXT="Tous fichier &quot;joints&quot;" ID="ID_361192813" CREATED="1484125255985" MODIFIED="1484125269628"/>
+</node>
+<node TEXT="educanet2" ID="ID_1910465811" CREATED="1484125098761" MODIFIED="1484125142791">
+<node TEXT="Connexion" ID="ID_318944794" CREATED="1484125123331" MODIFIED="1484125142790">
+<node TEXT="Login (adresse educanet2 valide)" ID="ID_983357407" CREATED="1484125131492" MODIFIED="1484125152664"/>
+<node TEXT="Password" ID="ID_1179118096" CREATED="1484125135486" MODIFIED="1484125137696"/>
+</node>
+</node>
+<node TEXT="Quoi?" ID="ID_1047136531" CREATED="1484125274891" MODIFIED="1484125293719">
+<node TEXT="Nom" ID="ID_1038308864" CREATED="1484125344833" MODIFIED="1484125349852"/>
+<node TEXT="Adresse" ID="ID_1780004506" CREATED="1484125356523" MODIFIED="1484125364885"/>
+<node TEXT="Date de cr&#xe9;ation" ID="ID_1249998279" CREATED="1484125325616" MODIFIED="1484125331627"/>
+<node TEXT="Date de modification" ID="ID_292302264" CREATED="1484125519891" MODIFIED="1484125524806"/>
+<node TEXT="Autheur" ID="ID_1007565742" CREATED="1484125333056" MODIFIED="1484125342419"/>
+</node>
+</node>
+<node TEXT="BD" POSITION="right" ID="ID_1771252028" CREATED="1484125190490" MODIFIED="1484125623496" HGAP_QUANTITY="-89.49999691545972 pt" VSHIFT_QUANTITY="80.24999760836369 pt">
+<edge COLOR="#00ffff"/>
+<node TEXT="Tables" ID="ID_1923435594" CREATED="1484125458069" MODIFIED="1484125461952">
+<node TEXT="Dossiers" ID="ID_87204967" CREATED="1484125464549" MODIFIED="1484125474081">
+<node TEXT="id" ID="ID_859423329" CREATED="1484125486776" MODIFIED="1484125493659"/>
+<node TEXT="Nom" ID="ID_1269585015" CREATED="1484125494504" MODIFIED="1484125497995"/>
+</node>
+<node TEXT="Fichiers" ID="ID_1762587020" CREATED="1484125500049" MODIFIED="1484125506093">
+<node TEXT="id" ID="ID_1918976497" CREATED="1484125507522" MODIFIED="1484125509093"/>
+<node TEXT="Nom" ID="ID_120065155" CREATED="1484125510058" MODIFIED="1484125511644"/>
+<node TEXT="Date de cr&#xe9;ation" ID="ID_1421768709" CREATED="1484125512082" MODIFIED="1484125532247"/>
+<node TEXT="Date de modification" ID="ID_134521113" CREATED="1484125532676" MODIFIED="1484125536232"/>
+<node TEXT="Autheur" ID="ID_1910671293" CREATED="1484125607694" MODIFIED="1484125610328"/>
+<node TEXT="dossier parent" ID="ID_1925052923" CREATED="1484125536701" MODIFIED="1484125546153"/>
+</node>
+<node TEXT="Dossiers-Dossiers" ID="ID_1729826788" CREATED="1484125550478" MODIFIED="1484125556913">
+<node TEXT="id dossier parent" ID="ID_650456795" CREATED="1484125559078" MODIFIED="1484125565713"/>
+<node TEXT="id dossier enfant" ID="ID_1191475056" CREATED="1484125566134" MODIFIED="1484125573152"/>
+</node>
+</node>
+</node>
+</node>
+</map>
