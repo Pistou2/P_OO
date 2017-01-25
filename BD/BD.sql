804c1fa1a00fae157497544f9410531010387a12
merkya <merkya@W10-1>
2017-01-25 11:26:48 +0100

    TODO

--- BD/BD.sql
+++ BD/BD.sql
@@ -1,22 +1,7 @@
-#------------------------------------------------------------#
-#ETML
-#Author  : Merk Yann
-#Date    : 18.01.2017
-#Summary : P_OO, database creation script
-#------------------------------------------------------------#
-
-DROP DATABASE IF EXISTS db_oo;
-CREATE DATABASE db_oo;
-
-USE db_oo;
-
-
-#------------------------------------------------------------
-# Table: t_files
-#------------------------------------------------------------
-
+DROP TABLE IF EXISTS t_files;
+DROP TABLE IF EXISTS t_folder;
 CREATE TABLE t_files(
-        idFile              int (11) Auto_increment  NOT NULL ,
+        idFile              int (11) NOT NULL PRIMARY KEY ,
         filName             Varchar (100) NOT NULL ,
         filCreationDate     Date NOT NULL ,
         filModificationDate Date ,
@@ -24,21 +9,14 @@ CREATE TABLE t_files(
         filTextContent      Varchar (10000) ,
         filStillExist       Boolean NOT NULL ,
         idFolder            Int NOT NULL ,
-        PRIMARY KEY (idFile )
-)ENGINE=InnoDB;
+		FOREIGN KEY (idFolder) REFERENCES t_folder(idFolder)
+);
 
-
-#------------------------------------------------------------
-# Table: t_folder
-#------------------------------------------------------------
 
 CREATE TABLE t_folder(
-        idFolder      int (11) Auto_increment  NOT NULL ,
+        idFolder      int (11) NOT NULL PRIMARY KEY ,
         folName       Varchar (50) NOT NULL ,
         folStillExist Boolean NOT NULL ,
         idParent    Int ,
-        PRIMARY KEY (idFolder )
-)ENGINE=InnoDB;
-
-ALTER TABLE t_files ADD CONSTRAINT FK_t_files_idFolder FOREIGN KEY (idFolder) REFERENCES t_folder(idFolder);
-ALTER TABLE t_folder ADD CONSTRAINT FK_t_folder_idParent FOREIGN KEY (idParent) REFERENCES t_folder(idFolder);
+		FOREIGN KEY (idParent) REFERENCES t_folder(idFolder)
+);
