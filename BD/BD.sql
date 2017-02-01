0ea8738f875455dcf80dcf881508216a35204ac8
merkya <merkya@W10-1>
2017-02-01 08:42:08 +0100

    Added a type of folder in t_folder

--- BD/BD.sql
+++ BD/BD.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS t_folder;
 CREATE TABLE t_files(
         idFile              int (11) NOT NULL PRIMARY KEY ,
         filName             Varchar (100) NOT NULL ,
+		filExtension		Varchar (30) NOT NULL,
         filCreationDate     Date NOT NULL ,
         filModificationDate Date ,
         filAuthor           Varchar (40) ,
@@ -17,6 +18,7 @@ CREATE TABLE t_folder(
         idFolder      int (11) NOT NULL PRIMARY KEY ,
         folName       Varchar (50) NOT NULL ,
         folStillExist Boolean NOT NULL ,
+		folType		int(11) NOT NULL,
         idParent    Int ,
 		FOREIGN KEY (idParent) REFERENCES t_folder(idFolder)
 );
